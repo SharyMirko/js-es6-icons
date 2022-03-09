ab19382c5d36b58552f4f60353beadb297aa889d
Mirko Moschella <shary.mirko@gmail.com>
2022-03-09 15:09:10 +0100

    suddivido le icone per semplificarmi l'estrazione

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-[
+const arrIcon = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -112,3 +112,61 @@
 		color: 'blue'
 	}
 ];
+const arrTypeIcon = []
+/* Milestone 1
+Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
+Milestone 2
+Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+Milestone 3
+Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+BONUS
+1- modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
+2- popolare le options della select della milestone 3 dinamicamente. */
+const containerIcon = document.getElementById('container');
+const selectType = document.getElementById('icon-type');
+const iconUser = []
+const iconAnimal = []
+const iconVegetable = []
+
+arrIcon.forEach(ele => {
+	if(!arrTypeIcon.includes(ele.type)) {
+		arrTypeIcon.push(ele.type)
+	}
+})
+
+for (i = 0; i < arrIcon.length; i++){
+	if(arrIcon[i].type == 'animal') {
+		iconAnimal.push(arrIcon[i])
+	} else if(arrIcon[i].type == 'vegetable') {
+		iconVegetable.push(arrIcon[i])
+	} else if(arrIcon[i].type == 'user') {
+		iconUser.push(arrIcon[i])
+	}
+}
+
+for(let i = 0; i < arrIcon.length; i++) {
+	containerIcon.innerHTML += `<i class="${arrIcon[i].family} ${arrIcon[i].prefix}${arrIcon[i].name}" style="color: ${arrIcon[i].color}"></i>`
+};
+
+selectType.addEventListener('change', typeSwitch)
+
+function typeSwitch() {
+	containerIcon.innerHTML = ''
+	if(selectType.value == 'animal') {
+		for (let i in iconAnimal) {
+			containerIcon.innerHTML += `<i class="${iconAnimal[i].family} ${iconAnimal[i].prefix}${iconAnimal[i].name}" style="color: ${iconAnimal[i].color}"></i>`
+		}
+	} else if (selectType.value == 'vegetable') {
+		for (let i in iconVegetable) {
+			containerIcon.innerHTML += `<i class="${iconVegetable[i].family} ${iconVegetable[i].prefix}${iconVegetable[i].name}" style="color: ${iconVegetable[i].color}"></i>`
+		}
+	} else if(selectType.value == 'user') {
+		for (let i in iconUser) {
+			containerIcon.innerHTML += `<i class="${iconUser[i].family} ${iconUser[i].prefix}${iconUser[i].name}" style="color: ${iconUser[i].color}"></i>`
+		}
+	} else {
+		for(let i = 0; i < arrIcon.length; i++) {
+			containerIcon.innerHTML += `<i class="${arrIcon[i].family} ${arrIcon[i].prefix}${arrIcon[i].name}" style="color: ${arrIcon[i].color}"></i>`
+		};
+	}
+}
